9ac1ee9a3b70df260ca590785383ddc596df080c
R. Bramaditya <bramadityaw@duck.com>
2025-02-20 16:28:57 +0700

    implement tls

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,28 +1,53 @@
-use tokio::{
-    io::AsyncReadExt,
-    net::{TcpListener, TcpStream},
+use backend;
+use std::net::SocketAddr;
+use std::{fs::File, io::Read};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_native_tls::{
+    native_tls::{self, Identity},
+    TlsStream,
 };
-use tokio_tungstenite::tungstenite::Result;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> backend::Result<()> {
     let addr = "127.0.0.1:3000";
     let listener = TcpListener::bind(addr)
         .await
         .expect(&format!("Failed to bind to {addr}"));
 
+    // TODO: Generalize the cert path
+    let cert_path = "/home/bram/.localhost/127.0.0.1";
+
+    let mut pem_file = File::open(format!("{cert_path}.pem")).expect("Fix hardcoded path");
+    let mut pem = vec![];
+    pem_file.read_to_end(&mut pem)?;
+
+    let mut key_file = File::open(format!("{cert_path}-key.pem")).expect("Fix hardcoded path");
+    let mut key = vec![];
+    key_file.read_to_end(&mut key)?;
+
+    let identity = Identity::from_pkcs8(&pem, &key)?;
+    let tls_acceptor =
+        tokio_native_tls::TlsAcceptor::from(native_tls::TlsAcceptor::builder(identity).build()?);
+
     println!("Listening on {addr}");
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(handle_stream(stream));
+    while let Ok((stream, peer)) = listener.accept().await {
+        let tls = tls_acceptor.clone();
+        tokio::spawn(async move {
+            match tls.accept(stream).await {
+                Ok(s) => accept_connection(peer, s).await,
+                Err(e) => Err(backend::Error::TlsErr(e)),
+            }
+        });
     }
 
     Ok(())
 }
 
-async fn handle_stream(mut stream: TcpStream) -> Result<()> {
-    let mut buf: &mut [u8] = &mut [0; 1024];
-    stream.read(&mut buf).await?;
-    print!("{:x?}", buf);
+async fn accept_connection(peer: SocketAddr, stream: TlsStream<TcpStream>) -> backend::Result<()> {
+    let mut ws = tokio_tungstenite::accept_async(stream).await?;
+    println!("Connected to peer at {} {}", peer.ip(), peer.port());
+
+    ws.close(None).await?;
 
     Ok(())
 }
--- frontend/serve.sh
+++ frontend/serve.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 set -e
-cert_path="$HOME/.localhost/localhost"
+cert_path="$HOME/.localhost/127.0.0.1"
 http-server -S -C "$cert_path.pem" -K "$cert_path-key.pem"
